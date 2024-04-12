3eca3bbb3d7fdd8a116924e0c6ad94009961e3f4
Raul C Siqueira <raulcesarsiqueira@gmail.com>
2024-04-12 20:50:08 -0300

    added test

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -1,24 +1,12 @@
-import '@testing-library/jest-dom';
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-
 import MainContent from './MainContent';
 
-// My tests!
-// Add yet another comment
-
 describe('MainContent', () => {
-  it('should render a button', () => {
+  test('renders MainContent component', () => {
     render(<MainContent />);
 
-    expect(screen.getByRole('button')).toBeInTheDocument();
-  });
-
-  it('should show the help area after clicking the button', async () => {
-    render(<MainContent />);
-
-    const button = screen.getByRole('button');
-    await userEvent.click(button);
-    expect(screen.getByTestId('help-area')).toBeInTheDocument();
+    // Suponha que o MainContent renderize um elemento com o texto "Conteúdo Principal"
+    expect(screen.getByText('Conteúdo Principal')).toBeInTheDocument();
   });
 });
